e9691a794ea2ab5f18d5b657b797d759ad6a0cf1
thde <thde@users.noreply.github.com>
2020-12-08 17:03:13 +0100

    add artist domain verification

--- api/_bandcamp.ts
+++ api/_bandcamp.ts
@@ -1,6 +1,10 @@
 import * as Bandcamp from 'bandcamp-scraper'
+import { promises as dns } from 'dns'
+import { CONFIG } from './_config'
 
 export function getReleases(artist: string) {
+  verifyArtist(artist)
+
   return toPromise<string, string[]>(
     Bandcamp.getAlbumUrls,
     `https://${artist}`
@@ -22,6 +26,7 @@ export interface ReleaseParams {
 }
 
 export async function getRelease({ artist, type, name }: ReleaseParams) {
+  verifyArtist(artist)
   const url = `https://${artist}/${type}/${name}`
 
   const info = await toPromise<string, string[]>(Bandcamp.getAlbumInfo, url)
@@ -31,6 +36,15 @@ export async function getRelease({ artist, type, name }: ReleaseParams) {
   )
 
   return { ...info, products: products }
+}
+
+async function verifyArtist(artist: string) {
+  if (artist.endsWith('.bandcamp.com')) return
+
+  const address = (await dns.lookup(artist)).address
+  if (CONFIG.Bandcamp.AllowedIps.includes(address)) return
+
+  throw 'Artist domain not valid!'
 }
 
 function toPromise<P, R>(
